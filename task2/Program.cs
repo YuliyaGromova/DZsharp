b4ebc670f0cfc556b55e747e0dd50d0480e159ee
Юленька <chervyak_9@mail.ru>
2022-08-05 01:16:37 +0300

    Задача 2: вывести максимальное из трех чисел

---
+++ task2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдает максимальное из чисел.
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int d = Convert.ToInt32(Console.ReadLine());
+int maximum = a;
+if (b>maximum)
+{
+    maximum = b;
+}
+if (d>maximum)
+{
+    maximum = d;
+}
+Console.WriteLine($"Максимальное число = {maximum} ");
