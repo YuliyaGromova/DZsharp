d1e3d3633e6d350fbd6da2ce2f6022df31859aa9
Юленька <chervyak_9@mail.ru>
2022-08-05 00:54:46 +0300

    задача 1: сравнение двух чисел

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает два числа и выдает, какое число больше, а какое меньше.
+
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a>b)
+{
+    Console.WriteLine($"Первое число ({a}) больше второго числа ({b})");
+}
+else if (b>a)
+{
+    Console.WriteLine($"Второе число ({b}) больше первого числа ({a})");
+}
+else
+{
+    Console.WriteLine($"Первое число ({a}) равно второму числу ({b})");
+}
