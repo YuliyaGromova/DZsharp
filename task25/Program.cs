462712489cd3eaef474780bc4be0321543b23814
Юленька <chervyak_9@mail.ru>
2022-08-16 23:49:05 +0300

    задача 25: возведение в степень. доработан универсальный метод

--- task25/Program.cs
+++ task25/Program.cs
@@ -7,7 +7,7 @@ int num = Convert.ToInt32(Console.ReadLine());
 Console.Write("Вкакую степень возвести число? ");
 int step = Convert.ToInt32(Console.ReadLine());
 
-string PowerNumber(int number, int stepen)
+string PowerNumber(int number, int stepen)    // универсальный метод 
 {
     double res = 1;
     if (number != 0)
@@ -16,17 +16,22 @@ string PowerNumber(int number, int stepen)
         {
             for (int i = 0; i < stepen; i++)
             {
-                if (stepen > 0) res = res * number;
+                res = res * number;
             }
         }
-        if (stepen < 0) 
+        if (stepen < 0)
         {
             for (int i = 0; i < -stepen; i++) res = res / number;
         }
+        if (stepen == 0 && number < 0) res = -1;
     }
-    else return("Возведение числа 0 в степень не имеет смысла");
-return ($"Число {number} в степени {stepen} => {Math.Round(res,4, MidpointRounding.ToZero)}");
+    else return ("Возведение числа 0 в степень не имеет смысла");
+    return ($"Число {number} в степени {stepen} => {Math.Round(res, 10, MidpointRounding.ToZero)}");
 }
-
-string result = PowerNumber(num, step);
-Console.WriteLine(result);
+// применяем метод к конкретной задаче: только если степень - натуральное число
+if (step <= 0) Console.WriteLine("Некорректное значение степени.Отрицательные числа и ноль не являются натуральными числами");
+else
+{
+    string result = PowerNumber(num, step);
+    Console.WriteLine(result);
+}
---
+++ task27/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+Console.Write("Введите число : ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+string SumNumbers(int num){
+int numb=num;
+int last=0;
+int summa=0;
+if (num<0) num=-num;
+while (num>0)
+{
+    last=num%10;
+    num=num/10;
+    summa+=last;
+}
+return($"Сумма цифр числа {numb} => {summa}");
+}
+
+string res = SumNumbers(number);
+Console.WriteLine(res);
